9283f1669543cd94d8a71bc3a3043844d331d3a3
bharathkumarreddy14 <95518348+bharathkumarreddy14@users.noreply.github.com>
2021-12-04 11:35:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+const openNav = document.querySelector(".hamburger");
+const closeNav = document.querySelector(".close");
+const menu = document.querySelector(".menu");
+
+const leftPosition = menu.getBoundingClientRect().left;
+openNav.addEventListener("click", () => {
+  if (leftPosition < 0) {
+    menu.classList.add("show");
+
+  }
+});
+
+closeNav.addEventListener("click", () => {
+  if (leftPosition < 0) {
+    menu.classList.remove("show");
+  }
+}
+);
+function shop(){
+  alert("please select the product")
+}
+function mycoolfunction(){
+  alert("your product has been added to the cart. please fill the form mentioned below ")
+}
+
+
+  
